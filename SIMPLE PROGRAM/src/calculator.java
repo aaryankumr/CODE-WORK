c0bd50dd84fc40c8cb26b8a7015b2b056f84e473
aaryankumr <aaryan.sirmvit@gmail.com>
2026-06-16 20:05:33 +0530

    16062026

--- SIMPLE PROGRAM/src/calculator.java
+++ SIMPLE PROGRAM/src/calculator.java
@@ -1,2 +1,13 @@
+import java.util.Scanner;
+
 public class calculator {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Welcome to Simple Calculator");
+        System.out.println("Enter 1st Number: ");
+        float first= scan.nextFloat();
+        System.out.println("Enter 2nd Number: ");
+        float second = scan.nextFloat();
+
+    }
 }
